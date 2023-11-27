d7feb8ed9a889c98201d3ed352f31d80c8d9507b
techzsky <152121942+techzsky@users.noreply.github.com>
2023-11-27 07:34:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/dineshgitclone/RRbot.git /RRbot
+  git clone https://github.com/techzsky/RRbot.git /RRbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RRbot
